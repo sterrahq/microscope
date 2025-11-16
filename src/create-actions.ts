380723873670103a240b9245263fb37984071ed8
MAZE <remvze@proton.me>
2025-11-16 22:35:57 +0330

    feat: add support for async actions

--- src/create-actions.ts
+++ src/create-actions.ts
@@ -1,22 +1,31 @@
 import { StateUpdater, Store } from "./types";
 
-type ActionMap<T> = Record<string, (...args: any[]) => StateUpdater<T>>;
+type ActionMap<T> = Record<
+  string,
+  (...args: any[]) => StateUpdater<T> | Promise<StateUpdater<T>>
+>;
 
 export function createActions<T, A extends ActionMap<T>>(
   store: Store<T>,
   actions: A
 ): {
-  [K in keyof A]: (...args: Parameters<A[K]>) => void;
+  [K in keyof A]: ReturnType<A[K]> extends Promise<any>
+    ? (...args: Parameters<A[K]>) => Promise<void>
+    : (...args: Parameters<A[K]>) => void;
 } {
-  const wrapped: Partial<Record<keyof A, (...args: any[]) => void>> = {};
+  const wrapped: Partial<Record<keyof A, any>> = {};
 
   for (const key of Object.keys(actions) as Array<keyof A>) {
-    wrapped[key] = ((...args: any[]) => {
-      const updater = actions[key](...args);
+    wrapped[key] = (...args: any[]) => {
+      const result = actions[key](...args);
 
-      store.set(updater, key as string);
-    }) as (...args: Parameters<A[typeof key]>) => void;
+      if (result instanceof Promise) {
+        return result.then((updater) => store.set(updater, key as string));
+      } else {
+        store.set(result, key as string);
+      }
+    };
   }
 
-  return wrapped as { [K in keyof A]: (...args: Parameters<A[K]>) => void };
+  return wrapped as any;
 }
